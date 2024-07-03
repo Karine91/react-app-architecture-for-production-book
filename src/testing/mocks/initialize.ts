be2d8ef709dcd659dbe0dabe02be5e0567fd77f5
karine <gulkarine91@gmail.com>
2024-07-03 11:23:14 +0200

    chore: fix format

---
+++ src/testing/mocks/initialize.ts
@@ -0,0 +1,12 @@
+import { IS_SERVER } from '@/config/constants';
+export const initializeMocks = () => {
+  if (IS_SERVER) {
+    const { server } = require('./server');
+    server.listen();
+  } else {
+    const { worker } = require('./browser');
+    worker.start();
+  }
+};
+
+initializeMocks();
